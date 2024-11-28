3bb5f04faa134b49daddd42f5ee949ef90a9c1b8
Lee Hose <0707lee7@gmail.com>
2024-11-28 15:23:48 +0900

    Merge branch 'main' of https://github.com/Avy012/Catch_Mind_Game.git into main

--- CatchMind/src/Pen.java
+++ CatchMind/src/Pen.java
@@ -1,9 +1,13 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 
 public class Pen extends JFrame implements ActionListener {
@@ -15,28 +19,32 @@ public class Pen extends JFrame implements ActionListener {
 
     JPanel color_space, Canvas_space;
     JButton Red_button, Blue_button, Green_button, Yellow_button, Black_button, Clear_button;
+    JLabel timerLabel, quizLabel;
     CreateCanvas createcanvas;
+    CatchMindTimer catchmindtimer;
+    QuizWord Quizmanager;
 
     Pen(CreateCanvas createcanvas) {
         setSize(1000, 750);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null); 
+        setLayout(null);
 
         this.createcanvas = createcanvas;
 
+        // 캔버스 공간
         Canvas_space = new JPanel();
-        Canvas_space.setBounds(50, 50, 800, 500); 
+        Canvas_space.setBounds(175, 75, 600, 500);
         Canvas_space.setBorder(new LineBorder(Color.black));
-        Canvas_space.setLayout(null); 
-        createcanvas.setBounds(0, 0, 800, 500); 
+        Canvas_space.setLayout(null);
+        createcanvas.setBounds(0, 0, 800, 500);
         Canvas_space.add(createcanvas);
 
-
+        // 색상 버튼 공간
         color_space = new JPanel();
-        color_space.setBounds(50, 600, 800, 50);
+        color_space.setBounds(175, 600, 600, 50);
         color_space.setBorder(new LineBorder(Color.black));
 
-  
+        // 색상 버튼 생성
         Red_button = new JButton("빨강");
         Blue_button = new JButton("파랑");
         Green_button = new JButton("초록");
@@ -44,20 +52,51 @@ public class Pen extends JFrame implements ActionListener {
         Black_button = new JButton("검정");
         Clear_button = new JButton("지우기");
 
+        // 버튼 추가
         color_space.add(Black_button);
         color_space.add(Red_button);
         color_space.add(Yellow_button);
         color_space.add(Blue_button);
         color_space.add(Green_button);
-        color_space.add(Clear_button); 
+        color_space.add(Clear_button);
 
+        // 이벤트 리스너 추가
         Red_button.addActionListener(this);
         Blue_button.addActionListener(this);
         Green_button.addActionListener(this);
         Yellow_button.addActionListener(this);
         Black_button.addActionListener(this);
-        Clear_button.addActionListener(this); 
+        Clear_button.addActionListener(this);
 
+        // 타이머 레이블 추가
+        timerLabel = new JLabel("", SwingConstants.CENTER);
+        timerLabel.setBounds(50, 10, 250, 30);
+        timerLabel.setFont(new Font("System", Font.BOLD, 20));
+        timerLabel.setBorder(new LineBorder(Color.black));
+        add(timerLabel);
+
+        // 제시어 레이블 추가
+        quizLabel = new JLabel("", SwingConstants.CENTER);
+        quizLabel.setBounds(675, 10, 250, 30);
+        quizLabel.setFont(new Font("System", Font.BOLD, 20));
+        quizLabel.setBorder(new LineBorder(Color.black));
+        add(quizLabel);
+
+        
+        Quizmanager = new QuizWord(quizLabel);
+
+        // 타이머 설정
+        catchmindtimer = new CatchMindTimer(timerLabel, 10, () -> {
+            JOptionPane.showMessageDialog(Canvas_space, "게임 종료");
+            createcanvas.clearCanvas();
+            catchmindtimer.reset(60);
+            Quizmanager.setRandomword(); // 새로운 제시어
+        });
+
+        catchmindtimer.start(); // 타이머 시작
+
+        // 초기 제시어 설정
+        Quizmanager.setRandomword();
 
         add(Canvas_space);
         add(color_space);
@@ -80,7 +119,8 @@ public class Pen extends JFrame implements ActionListener {
         } else if (e.getSource() == Black_button) {
             createcanvas.set_color(Blackcolor);
         } else if (e.getSource() == Clear_button) {
-            createcanvas.clearCanvas(); 
+            createcanvas.clearCanvas();
+            Quizmanager.setRandomword(); //test
         }
     }
 
---
+++ CatchMind/src/QuizWord.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import javax.swing.JLabel;
+
+public class QuizWord {
+    private String[] prompts;
+    private JLabel promptLabel;
+
+    public QuizWord(JLabel promptLabel) {
+        this.promptLabel = promptLabel;
+        this.prompts = new String[] { "상어", "집", "자동차", "고양이", "강아지", "사과", "산", "바다", "사람", "책","두부" };
+    }
+
+    public void setRandomword() {
+        Random random = new Random();
+        String randomPrompt = prompts[random.nextInt(prompts.length)];
+        promptLabel.setText("제시어: " + randomPrompt);
+    }
+}
